cfd0bf72f683f7543d3191efff311ba9f42b8882
JosephMonteCarlo <DioBrando2003@yandex.ru>
2022-10-08 23:39:34 +0300

    Вроде бы все норм работает

---
+++ HTTPResponse/HTTPResponse/HttpServer.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Threading;
+using System.Net;
+using System.IO;
+using System.Text.Json;
+
+namespace HTTPResponse
+{
+    public class HttpServer : IDisposable
+    {
+        static HttpListener listener;
+        static Stream output;
+        public ServerStatus Status = ServerStatus.Stop;
+        string _path;
+        private readonly int _port;
+
+        public HttpServer()
+        {
+            if (File.Exists(Path.GetFullPath("Config.json")))
+            {
+                string fileName = "Config.json";
+                string jsonString = File.ReadAllText(fileName);
+                ServerSettings setting = JsonSerializer.Deserialize<ServerSettings>(jsonString);
+                _port = setting.Port;
+                _path = setting.Path;
+            }
+            else
+            {
+                ServerSettings setting = new ServerSettings(); 
+                _port = setting.Port;
+                _path = setting.Path;
+            }
+            listener = new HttpListener();
+            // установка адресов прослушки
+            listener.Prefixes.Add($"http://localhost:{_port}/");
+        }
+
+        public void Begin()
+        {
+            if (Status == ServerStatus.Start)
+            {
+                Console.WriteLine("Уже запущен");
+                return;
+            }
+            Console.WriteLine("Запуск сервера...");
+            new HttpServer();
+
+            listener.Start();
+            
+            Console.WriteLine("Ожидание подключений...");
+            Status = ServerStatus.Start;
+            // метод GetContext блокирует текущий поток, ожидая получение запроса
+            Listen();
+        }
+        public void Listen()
+        {
+            listener.BeginGetContext(new AsyncCallback(ListenCallback), listener);
+        }
+        public void ListenCallback(IAsyncResult result)
+        {
+            if (listener.IsListening)
+            {
+                var context = listener.EndGetContext(result);
+                HttpListenerRequest request = context.Request;
+                HttpListenerResponse response = context.Response;
+                var path = Directory.GetCurrentDirectory();
+                byte[] buffer;
+
+                if (Directory.Exists(path))
+                
+                {
+                    buffer = FileFinder.GetFile(context.Request.RawUrl.Replace("%20"," "), "index.html", _path);
+                   
+                    response.Headers.Set("Content-Type", "text/css");
+                    response.Headers.Add("Content-Type", "text/html");
+                    
+                   
+                    if (buffer == null)
+                    {
+                        response.Headers.Set("Content-Type", "text/plain");
+                        response.StatusCode = (int)HttpStatusCode.NotFound;
+                        string err = "404 - Not Found";
+                        buffer = System.Text.Encoding.UTF8.GetBytes(err);
+                    }
+                }
+                else
+                {
+                    
+                    string err = $"{path} is not found";
+                    buffer = System.Text.Encoding.UTF8.GetBytes(err);
+                    
+                }
+                
+                output = response.OutputStream;
+                output.Write(buffer, 0, buffer.Length);
+                output.Close();
+                Listen();
+            }
+        }
+        public void Stop()
+        {
+            if (Status == ServerStatus.Stop) return;
+            // останавливаем прослушивание подключений
+            listener.Abort();
+            Status = ServerStatus.Stop;
+            listener = new HttpListener();
+            Console.WriteLine("Обработка подключений завершена");
+        }
+        public void Dispose()
+        {
+            Stop();
+        }
+    }
+}
--- HTTPResponse/HTTPResponse/Program.cs
+++ HTTPResponse/HTTPResponse/Program.cs
@@ -5,108 +5,40 @@ using System.IO;
 
 namespace HTTPResponse
 {
-    class HttpServer
+    internal class Program
     {
-        private static object _lock = new object();
-        static HttpListener listener;
-        static Stream output;
-        static void Main(string[] args)
+        static bool _appIsRunning = true;
+        public static void Main()
         {
-            Thread cmd = new Thread(CMDListen);
-            cmd.Start();
-            
-            HttpServer.Begin();
-
-        }
-        public static void Begin()
-        {
-            listener = new HttpListener();
-            // установка адресов прослушки
-            listener.Prefixes.Add("http://localhost:8888/google/");
-            listener.Start();
-            Console.WriteLine("Ожидание подключений...");
-            // метод GetContext блокирует текущий поток, ожидая получение запроса
-            Listen();
-
-        }
-        public static void Listen()
-        {
-            try
+            using (var server = new HttpServer())
             {
-                while (true)
+                server.Begin();
+                while (_appIsRunning)
                 {
-                    var context = listener.GetContext();
-                    HttpListenerRequest request = context.Request;
-                    HttpListenerResponse response = context.Response;
-                    var responseString = File.ReadAllText("index.html");
-                    //var responseString = new FileStream(@"C:\Users\Lenovo\source\repos\HTTPResponse\HTTPResponse\google\index.html", FileMode.Open);
-                    
-
-                    //string responseString = "<html><head><meta charset='utf8'></head><body>Привет мир!</body></html>";
-                    byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
-                    response.ContentLength64 = buffer.Length;
-                    output = response.OutputStream;
-                    output.Write(buffer, 0, buffer.Length);
+                    CMDListen(Console.ReadLine()?.ToLower(), server);
                 }
             }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                Close();
-            }
-        }
-        public static void Close()
-        {
-            // закрываем поток
-            output?.Close();
-            // останавливаем прослушивание подключений
-            listener.Stop();
-            listener = null;
-            output = null;
-            Console.WriteLine("Обработка подключений завершена");
-            Console.Read();
-        }
 
-        private static void CMDListen()
+        }
+        public static void CMDListen(string command, HttpServer server)
         {
-            while (true)
-            {
-                var command = Console.ReadLine();
+
                 switch (command)
                 {
                     case "close":
-                        if (output != null)
-                            Close();
+                        server.Stop();
                         break;
                     case "start":
-                        if (listener == null)
-                        {
-                            lock (_lock)
-                            {
-                                if (listener == null)
-                                {
-                                    Thread startListener = new Thread(Begin);
-                                    startListener.Start();
-                                }
-                            }
-                        }
+                        server.Begin();
                         break;
                     case "restart":
-                        if (output != null && listener!=null)
-                        {
-                            Close();
-                            Thread startListener = new Thread(Begin);
-                            startListener.Start();
-                        }
-                        else
-                        {
-                            Thread startListener = new Thread(Begin);
-                            startListener.Start();
-                        }
+                        server.Stop();
+                        server.Begin();
                         break;
-
+                    case "exit":
+                        _appIsRunning = false;
+                        break;
                 }
-            }
         }
     }
 }
